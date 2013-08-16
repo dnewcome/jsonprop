dbabc5d5d143261bc05e284ea85bd0845d22af68
Dan Newcome <dan@dnuke.com>
2013-08-16 08:38:20 -0700

    removing standalone serialization function

--- json_dict.py
+++ json_dict.py
@@ -18,14 +18,3 @@ class JsonObject:
         return json.dumps(self._to_json_dict(cls, obj))
 
 
-def to_json_dict(cls, obj):
-    retval = {}
-    for key in cls.__dict__:
-        item = cls.__dict__[key] 
-        if isinstance(item, json_property):
-            if item._ser_if_null:
-                retval[item.prefix] = item.__get__(obj)
-            elif item.__get__(obj):
-                retval[item.prefix] = item.__get__(obj)
-
-    return retval 
--- test.py
+++ test.py
@@ -1,21 +1,19 @@
 from json_property import json_property   
-from json_dict import to_json_dict, JsonObject
+from json_dict import JsonObject
 import unittest
 
 
-class MyClass(object):
+class MyClass(JsonObject):
     def __init__(self):
         self._test_prop = None
-        self._is_serializing = False 
 
     @json_property('json_test_prop', False)
     def get_test_prop(self):
         return self._test_prop 
 
-class MyClassSerializeNulls(object):
+class MyClassSerializeNulls(JsonObject):
     def __init__(self):
         self._test_prop = None
-        self._is_serializing = False 
 
     @json_property('json_test_prop', True)
     def get_test_prop(self):
@@ -24,7 +22,6 @@ class MyClassSerializeNulls(object):
 class MyJsonObjectClass(JsonObject):
     def __init__(self):
         self._test_prop = None
-        self._is_serializing = False 
 
     @json_property('json_test_prop', False)
     def get_test_prop(self):
@@ -33,7 +30,6 @@ class MyJsonObjectClass(JsonObject):
 class JsonObjectTests(unittest.TestCase):
     def test_nominal(self):
         myclass = MyJsonObjectClass()
-        myclass._is_serializing = True
         myclass._test_prop = 'foo' 
 
         expected = {'json_test_prop': 'foo'}
@@ -43,7 +39,6 @@ class JsonObjectTests(unittest.TestCase):
 
     def test_json_dumps(self):
         myclass = MyJsonObjectClass()
-        myclass._is_serializing = True
         myclass._test_prop = 'foo' 
 
         expected = '{"json_test_prop": "foo"}'
@@ -55,28 +50,25 @@ class JsonPropertyTests(unittest.TestCase):
 
     def test_nominal(self):
         myclass = MyClass()
-        myclass._is_serializing = True
         myclass._test_prop = 'foo' 
 
         expected = {'json_test_prop': 'foo'}
-        actual = to_json_dict(MyClass, myclass)
+        actual = myclass._to_json_dict(MyClass, myclass)
 
         self.assertEqual(actual, expected) 
 
     def test_excluded(self):
         myclass = MyClass()
-        myclass._is_serializing = True
 
         expected = {}
-        actual = to_json_dict(MyClass, myclass)
+        actual = myclass._to_json_dict(MyClass, myclass)
 
         self.assertEqual(actual, expected) 
     
     def test_serialize_null(self):
         myclass = MyClassSerializeNulls()
-        myclass._is_serializing = True
 
         expected = {'json_test_prop': None}
-        actual = to_json_dict(MyClassSerializeNulls, myclass)
+        actual = myclass._to_json_dict(MyClassSerializeNulls, myclass)
 
         self.assertEqual(actual, expected) 
