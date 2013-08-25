1945514bf1a68c0da1d7b20fbeb363e7fb40b5f9
Dan Newcome <dan@dnuke.com>
2013-08-24 17:34:19 -0700

    mixed up naming in kwargs/recursive tests

--- jsonprop/test_kwargs.py
+++ jsonprop/test_kwargs.py
@@ -1,23 +1,17 @@
-import json
 from json_field import json_field
 from json_dict import JsonObject
-from json_encoder import JsonObjectEncoder
 import unittest
 
 class MyClass(JsonObject):
-    child = json_field('json_child', None, False)
-
-class MyClass2(JsonObject):
     json_test = json_field('json_test_prop', 'foo', False)
 
 class JsonFieldTests(unittest.TestCase):
 
     def test_kwargs(self):
-        mychild = MyClass2()
-        myparent = MyClass( child = mychild )
+        myclass = MyClass( json_test = 'bar' )
 
-        expected = '{"json_child": {"json_test_prop": "foo"}}'
-        actual = json.dumps( myparent._to_json_dict(), cls=JsonObjectEncoder)
+        expected = {'json_test_prop': 'bar'}
+        actual = myclass._to_json_dict()
 
         self.assertEqual(actual, expected) 
 
